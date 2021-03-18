74d45a8e3ff5ad984570a18bd8ff04c6e4ca0dd4
jorge <jorgelmr@outlook.es>
2021-03-18 08:42:19 -0500

    solucion parcial

---
+++ src/veterinaria/Perros.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package veterinaria;
+
+/**
+ *
+ * @author s114e13
+ */
+public class Perros extends Veterinaria{
+
+    public Perros(String nombre, int edad, String sexo, String raza) {
+        super(nombre, edad, sexo, raza, 10);
+    }
+
+}
